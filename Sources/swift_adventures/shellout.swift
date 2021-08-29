ca3febc76631d90528491fd930809f1be82b306f
Brandon Kurtz <kurtz.brandon@gmail.com>
2021-08-29 14:47:18 -0400

    shellout borrowed from nudge

---
+++ Sources/swift_adventures/shellout.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+@available (macOS 10.13, *)
+// Borrow with <3 from: https://github.com/macadmins/nudge/blob/main/Nudge/Utilities/SoftwareUpdate.swift
+func shellout(command: String, args: [String]) -> String {
+    let task = Process()
+    task.launchPath = command
+    task.arguments = args
+    let outputPipe = Pipe()
+    let errorPipe = Pipe()
+
+    task.standardOutput = outputPipe
+    task.standardError = errorPipe
+
+    do {
+        try task.run()
+    } catch {
+        return "Error running \(command) with args: \(args)"
+    }
+
+    task.waitUntilExit()
+
+    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+
+    let output = String(decoding: outputData, as: UTF8.self)
+    let error = String(decoding: errorData, as: UTF8.self)
+
+    if task.terminationStatus != 0 {
+        return error
+    } else {
+        return output
+    }
+ }
